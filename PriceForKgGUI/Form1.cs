1ad1157364ff7e9fc730859222e084d1e8ba7b91
Bozhidar <bozhidarrr@gmail.com>
2017-04-10 19:15:10 +0300

    Added dual price
    Added dual price calculator for comparing 2 items

--- PriceForKgGUI/Form1.cs
+++ PriceForKgGUI/Form1.cs
@@ -12,10 +12,40 @@ namespace PriceForKgGUI
 {
     public partial class Form1 : Form
     {
-        double price, amount,result,amountMultiplyer;        
+        double price, amount,result,amountMultiplyer;
+        double price2, amount2, result2, amountMultiplyer2;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            price2 = Convert.ToDouble(TxtPrice2.Text);
+            amount2 = Convert.ToDouble(TxtAmount2.Text);
+            if (price2 > 0 && amount2 > 0)
+            {
+                if (BtnGram2.Checked)
+                {
+                    amountMultiplyer2 = 1000 / amount2;
+                    result2 = amountMultiplyer2 * price2;
+                }
+                else if (BtnKilo2.Checked)
+                {
+                    amountMultiplyer2 = amount2;
+                    result2 = amountMultiplyer2 * price2;
+                }
+                else
+                {
+                    MessageBox.Show("Please select measurment unit", "Error", MessageBoxButtons.OK);
+                }
+                LblResult2.Text = result2.ToString("0.00 lv");
+            }
+            else
+            {
+                MessageBox.Show("Enter valid numbers", "Error", MessageBoxButtons.OK);
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -48,7 +78,7 @@ namespace PriceForKgGUI
                 {
                     MessageBox.Show("Please select measurment unit","Error",MessageBoxButtons.OK);
                 }
-                LblResult.Text = result.ToString("0.00 lev");
+                LblResult.Text = result.ToString("0.00 lv");
             }
             else
             {
